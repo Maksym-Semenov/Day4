0b2afeec870c0c36c79c0df2ed0d2168273ed87b
DeathForRussia <ramirkiev@gmail.com>
2023-02-04 14:21:38 +0200

    Add project files.

---
+++ Day4/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] values = { 10, 123, 56, 39, 67, 188 };
+int result = 0;
+for (int i = 0; i < values.Length; i++)
+{
+    result += values[i]; 
+}
+Console.WriteLine("Сума вашого масиву " + result);
+Console.ReadLine();
---
+++ ToDoApplication/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoApplication
+{    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] tasks = new string[3];
+        public MainWindow()
+        {
+            InitializeComponent();
+            tasks[0] = "Купити молоко";
+            tasks[1] = "Вивчити C#";
+            tasks[2] = "Створити резюме";
+
+            ToDoListBox.ItemsSource = tasks;
+        }
+    }
+}
